4b5554dd01736a6a4b19f18a91acd9cfc3c9f814
mihael-delac2000 <mihael.delac@happening.xyz>
2023-12-08 15:17:55 +0100

    day-04-first-done

---
+++ Day-04/first.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+
+const filePath = "input.txt";
+
+fs.readFile(filePath, "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const lines = data.split("\n");
+
+  let sum = 0;
+
+  for (const line of lines) {
+    const currentLine = line.split(/[|:]/).splice(1);
+    let winningNumbers, numbersList;
+    let matchingNumbers = [];
+
+    currentLine.forEach((str) => {
+      str = str.split(" ");
+      str = str.filter((value) => value !== "");
+      str = str.map(Number);
+      str.length == 10 ? (winningNumbers = str) : (numbersList = str);
+    });
+
+    winningNumbers.some((num) => {
+      numbersList.includes(num) && matchingNumbers.push(num);
+    });
+
+    const points = matchingNumbers.length && 2 ** (matchingNumbers.length - 1);
+    sum += points;
+  }
+
+  console.log("Sum: ", sum);
+});
